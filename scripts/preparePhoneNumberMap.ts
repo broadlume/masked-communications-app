be636fbafc4aa82ba31e3d758c38f2ec58ba269e
Eric Pfister <e_pfister55+github@outlook.com>
2024-07-29 21:12:03 +0000

    feat: better error handling in prepare script

--- scripts/preparePhoneNumberMap.ts
+++ scripts/preparePhoneNumberMap.ts
@@ -1,59 +1,61 @@
-import * as fsp from 'fs/promises'
-import path from 'path'
-import areaCodeGeos from './areaCodeGeos'
+import * as fsp from "fs/promises";
+import path from "path";
+import areaCodeGeos from "./areaCodeGeos";
 
 /****************************************************
  Formats a text list of phone numbers (in src/data/phone-numbers.txt) into the PhoneNumberMap
  structure.
 ****************************************************/
 
-async function preparePhoneNumberMap () {
-  const caAreaCodes = areaCodeGeos.ca.map(({ areaCode }) => areaCode)
-  const usAreaCodes = areaCodeGeos.us.map(({ areaCode }) => areaCode)
-
-  const phoneNumberList = (
-    await fsp.readFile(
-      path.join(__dirname, '../phoneNumbers.txt'),
-      'utf8'
-    )
-  ).split('\n')
-
-  const phoneNumberPoolMap = {
-    ca: {},
-    us: {}
+async function preparePhoneNumberMap() {
+	const caAreaCodes = areaCodeGeos.ca.map(({ areaCode }) => areaCode);
+	const usAreaCodes = areaCodeGeos.us.map(({ areaCode }) => areaCode);
+	let phoneNumberList: string[];
+	try {
+		phoneNumberList = (
+			await fsp.readFile(path.join(__dirname, "../phoneNumbers.txt"), "utf8")
+		).split("\n");
+	} catch (e) {
+    console.error("Could not read 'phoneNumbers.txt' - does it exist?");
+    console.info("'phoneNumbers.txt' must contain a newline-delimited list of E164 phone numbers for the number pool");
+    throw e;
   }
+  console.info(`Preparing ${phoneNumberList.length} phone numbers for the proxy pool...`);
+	const phoneNumberPoolMap = {
+		ca: {},
+		us: {},
+	};
 
-  for (const phoneNumber of phoneNumberList) {
-    const { countryCode, areaCode } =
-      phoneNumber.match(
-        /^\s*(?:\+?(?<countryCode>\d{1,3}))?[-. (]*(?<areaCode>\d{3})[-. )]*(?<prefix>\d{3})[-. ]*(?<line>\d{4})(?: *x(\d+))?\s*$/
-      ).groups
+	for (const phoneNumber of phoneNumberList) {
+		const { countryCode, areaCode } = phoneNumber.match(
+			/^\s*(?:\+?(?<countryCode>\d{1,3}))?[-. (]*(?<areaCode>\d{3})[-. )]*(?<prefix>\d{3})[-. ]*(?<line>\d{4})(?: *x(\d+))?\s*$/,
+		).groups;
 
-    if (!areaCode) throw Error(`${phoneNumber} is not valid E.164 format`)
+		if (!areaCode) throw Error(`${phoneNumber} is not valid E.164 format`);
 
-    // Check if area code is Canadian
-    if (caAreaCodes.includes(parseInt(areaCode))) {
-      // If the area code has an entry in our number pool output...
-      if (phoneNumberPoolMap.ca[areaCode]) {
-        // Push the number into the existing array
-        phoneNumberPoolMap.ca[areaCode].push(phoneNumber)
-        // Otherwise create a new entry and array
-      } else phoneNumberPoolMap.ca[areaCode] = [phoneNumber]
-    } else if (usAreaCodes.includes(parseInt(areaCode))) {
-      if (phoneNumberPoolMap.us[areaCode]) {
-        phoneNumberPoolMap.us[areaCode].push(phoneNumber)
-      } else phoneNumberPoolMap.us[areaCode] = [phoneNumber]
-    } else {
-      if (phoneNumberPoolMap[countryCode]) {
-        phoneNumberPoolMap[countryCode].push(phoneNumber)
-      } else phoneNumberPoolMap[countryCode] = [phoneNumber]
-    }
-  }
+		// Check if area code is Canadian
+		if (caAreaCodes.includes(parseInt(areaCode))) {
+			// If the area code has an entry in our number pool output...
+			if (phoneNumberPoolMap.ca[areaCode]) {
+				// Push the number into the existing array
+				phoneNumberPoolMap.ca[areaCode].push(phoneNumber);
+				// Otherwise create a new entry and array
+			} else phoneNumberPoolMap.ca[areaCode] = [phoneNumber];
+		} else if (usAreaCodes.includes(parseInt(areaCode))) {
+			if (phoneNumberPoolMap.us[areaCode]) {
+				phoneNumberPoolMap.us[areaCode].push(phoneNumber);
+			} else phoneNumberPoolMap.us[areaCode] = [phoneNumber];
+		} else {
+			if (phoneNumberPoolMap[countryCode]) {
+				phoneNumberPoolMap[countryCode].push(phoneNumber);
+			} else phoneNumberPoolMap[countryCode] = [phoneNumber];
+		}
+	}
 
-  await fsp.writeFile(
-    path.resolve(__dirname, '../src/data/phoneNumberMap.json'),
-    JSON.stringify(phoneNumberPoolMap)
-  )
+	await fsp.writeFile(
+		path.resolve(__dirname, "../src/data/phoneNumberMap.json"),
+		JSON.stringify(phoneNumberPoolMap),
+	);
 }
 
-preparePhoneNumberMap()
+preparePhoneNumberMap();
