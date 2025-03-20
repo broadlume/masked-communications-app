a3aac5b765de98198ff2cf79e7f29d7cae1c7ddd
Eric Pfister <e_pfister55+github@outlook.com>
2025-03-20 19:55:00 +0000

    feat: convert to lambda

--- index.ts
+++ index.ts
@@ -1,15 +1,18 @@
 // Copyright (c) 2022 Twilio Inc.
 
 import {app} from './src/app/app'
-
-import phoneNumberMap from "./src/data/phoneNumberMap.json"
-const PORT = process.env.PORT || 3000
+import serverlessExpress from "@codegenie/serverless-express"
 
 /****************************************************
  Start Server
  ****************************************************/
 
-app.listen(PORT, () => {
-  const phoneNumbers: string[] = Object.keys(phoneNumberMap).flatMap(country => Object.keys(phoneNumberMap[country]).flatMap(areacode => phoneNumberMap[country][areacode]));
-  console.log(`Twilio proxy is running on port:${PORT} with ${phoneNumbers.length} numbers in the pool.`);
-})
+// app.listen(PORT, () => {
+//   const phoneNumbers: string[] = Object.keys(phoneNumberMap).flatMap(country => Object.keys(phoneNumberMap[country]).flatMap(areacode => phoneNumberMap[country][areacode]));
+//   console.log(`Twilio proxy is running on port:${PORT} with ${phoneNumbers.length} numbers in the pool.`);
+// })
+
+export const handler = serverlessExpress({
+  app,
+  respondWithErrors: true,
+});
